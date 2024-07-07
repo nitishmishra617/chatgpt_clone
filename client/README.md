76ba1b87075ac4f4f89d62b7d58d76119c4f7825
Nitish Mishra <51686940+nitishmishra617@users.noreply.github.com>
2024-07-07 13:43:26 +0530

    Add files via upload

---
+++ client/README.md
@@ -0,0 +1,1 @@
+This is a chat gpt clone using gpt 3.5 api.
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import { borderRadius } from "@mui/system";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "fixed",
+        bottom: "0",
+        left: "0",
+        right: "0",
+      }}
+    >
+      <span
+        style={{ marginRight: "5px", fontSize: "1.2rem", fontWeight: "bold" }}
+      >
+        {`<`}
+        {`/`}
+        {`>`}
+      </span>
+      <p style={{ fontWeight: "bold" }}>
+        Developed by
+        <a
+          href="https://github.com/nitishmishra617"
+          target="blank"
+          style={{ padding: "5px" }}
+        >
+          Nitish Mishra
+        </a>
+      </p>
+    </div>
+  );
+}
